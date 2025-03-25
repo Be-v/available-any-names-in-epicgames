2aca02340a8b41900f7377a6f25f252710e39c42
Bev <yyypjp@gmail.com>
2025-03-25 22:54:07 +0300

    Add files via upload
    للموقع

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
+const letterMap = {
+    '3': 'З', 'A': 'А', 'a': 'а', 'X': 'Х', 'x': 'х',
+    'S': 'Ѕ', 's': 'ѕ', 'P': 'Р', 'p': 'р', 'O': 'О', 'o': 'о',
+    'J': 'Ј', 'j': 'ј', 'I': 'І', 'i': 'і', 'E': 'Е', 'e': 'е',
+    'C': 'С', 'c': 'с', 'B': 'В', 'T': 'Т', 'H': 'Н', 'M': 'М',
+    'K': 'К', 'y': 'у'
+};
+
 function convertName() {
-    let name = document.getElementById("nameInput").value;
-    let conversionMap = {
-        '3': 'З', 'A': 'А', 'a': 'а', 'X': 'Х', 'x': 'х',
-        'S': 'Ѕ', 's': 'ѕ', 'P': 'Р', 'p': 'р', 'O': 'О', 'o': 'о',
-        'J': 'Ј', 'j': 'ј', 'I': 'І', 'i': 'і', 'E': 'Е', 'e': 'е',
-        'C': 'С', 'c': 'с', 'B': 'В', 'T': 'Т',
-        'H': 'Н', 'M': 'М', 'K': 'К', 'y': 'у'
-    };
+    let inputName = document.getElementById("nameInput").value;
+    if (!inputName) return;
 
-    let convertedName = name.split('').map(char => {
-        return conversionMap[char] || char; 
-    }).join('');
+    let variations = [];
 
-    document.getElementById("result").textContent = `الاسم بعد التعديل: ${convertedName}`;
+    for (let i = 0; i < 3; i++) { // Generate 3 variations
+        let modifiedName = "";
+        for (let char of inputName) {
+            if (letterMap[char] && Math.random() > 0.3) { 
+                modifiedName += letterMap[char]; 
+            } else {
+                modifiedName += char;
+            }
+        }
+        variations.push(modifiedName);
+    }
+
+    document.getElementById("output").innerHTML = 
+        `<p>Modified Names:</p>
+         <p>${variations.join('<br>')}</p>`;
 }
 
-function saveToTxt() {
-    let text = document.getElementById("result").textContent;
+function saveToFile() {
+    let text = document.getElementById("output").innerText;
+    if (!text) return;
+
     let blob = new Blob([text], { type: "text/plain" });
     let link = document.createElement("a");
     link.href = URL.createObjectURL(blob);
-    link.download = "converted_name.txt";
+    link.download = "names.txt";
     link.click();
 }
